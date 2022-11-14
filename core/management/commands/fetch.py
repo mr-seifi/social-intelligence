807e9d8a657fe175202c4c3f723f3b7b9acd5b65
super amin <aminseiifi@gmail.com>
2022-11-14 16:14:37 +0330

    fix(command): fix data types when sending to the server

--- core/management/commands/fetch.py
+++ core/management/commands/fetch.py
@@ -1,7 +1,9 @@
+import re
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from core.services import LunarCrushService, InfluxDBService
-
+from influxdb_client.rest import ApiException
 
 class Command(BaseCommand):
     help = 'This command fetch social data from feed service and send it to the monitoring server'
@@ -15,8 +17,20 @@ class Command(BaseCommand):
         lunar_service = LunarCrushService(True if proxy else False)
         inf_service = InfluxDBService()
 
-        for sym in symbols:
+        for sym in symbols:  # TODO: Move this logic to the class
             df = lunar_service.fetch_data(asset_symbol=sym, interval='1w')
+            try:
+                inf_service.write(record=df, measurement_name=sym)
+                continue
+
+            except ApiException as ae:
+                field, measurement, type_1, type_2 = re.findall(r'input\sfield\s\\?\"(.+?)\\?\"\son\smeasurement\s\\?'
+                                                                r'\"(.+?)\\?\"\sis\stype\s(.+),\salready\sexists\sas'
+                                                                r'\stype\s(.+)\sdropped', str(ae))[0]
+                if type_2 == 'integer':
+                    type_2 = 'int'
+
+                df = df.astype({field: type_2})
             inf_service.write(record=df, measurement_name=sym)
 
         self.stdout.write("All data is successfully fetched!", ending='')
