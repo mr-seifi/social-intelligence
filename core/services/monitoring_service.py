65bed5e4cf32f7f41c7a7230d793561941d0976b
super amin <aminseiifi@gmail.com>
2022-11-15 12:23:25 +0330

    fix(monitoring): move exception logic to services.

--- core/services/monitoring_service.py
+++ core/services/monitoring_service.py
@@ -1,7 +1,9 @@
+import re
 import pandas as pd
 from django.conf import settings
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client.rest import ApiException
 
 
 class InfluxDBService:
@@ -21,6 +23,16 @@ class InfluxDBService:
         return self.client.write_api(write_options=SYNCHRONOUS)
 
     def write(self, record: pd.DataFrame, measurement_name):
-        self.write_client.write(bucket=self._bucket,
-                                record=record,
-                                data_frame_measurement_name=measurement_name)
+        try:
+            self.write_client.write(bucket=self._bucket,
+                                    record=record,
+                                    data_frame_measurement_name=measurement_name)
+        except ApiException as ae:
+            field, measurement, type_1, type_2 = re.findall(r'input\sfield\s\\?\"(.+?)\\?\"\son\smeasurement\s\\?'
+                                                            r'\"(.+?)\\?\"\sis\stype\s(.+),\salready\sexists\sas'
+                                                            r'\stype\s(.+)\sdropped', str(ae))[0]
+            if type_2 == 'integer':
+                type_2 = 'int'
+
+            record = record.astype({field: type_2})
+            self.write(record, measurement_name)
