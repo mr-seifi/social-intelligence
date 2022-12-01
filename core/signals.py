3e82fb1a28afd2f651ee09e6dc4f6326308af72c
super amin <aminseiifi@gmail.com>
2022-12-01 18:23:20 +0330

    fix(signal): add label value to gauge param

--- core/signals.py
+++ core/signals.py
@@ -6,9 +6,9 @@ from .models import Token
 
 @receiver(pre_save, sender=Token)
 def send_metrics_pre(sender, instance, *args, **kwargs):
-    token_social.dec(instance.social_volumes)
+    token_social.labels(instance.name).dec(instance.social_volumes)
 
 
 @receiver(post_save, sender=Token)
 def send_metrics_post(sender, instance, *args, **kwargs):
-    token_social.inc(instance.social_volumes)
+    token_social.labels(instance.name).inc(instance.social_volumes)
