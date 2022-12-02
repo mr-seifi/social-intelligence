5068c8e306c908f54168e1c74e2600541c044650
super amin <aminseiifi@gmail.com>
2022-12-02 03:46:45 +0330

    feat(prometheus-models): remove signals, models, and migrations

--- core/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Token(models.Model):
-    name = models.CharField(max_length=16, unique=True, db_index=True)
-    social_volumes = models.FloatField(null=True)
-    social_contributes = models.FloatField(null=True)
